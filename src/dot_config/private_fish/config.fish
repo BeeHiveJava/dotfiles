15078916f1b1370762fead98947a597daf5dc18c
BeeHiveJava <BeeHiveJava@users.noreply.github.com>
2024-05-09 11:20:22 +0000

    feat: add starship

--- src/dot_config/private_fish/config.fish
+++ src/dot_config/private_fish/config.fish
@@ -2,6 +2,11 @@ for file in $__fish_config_dir/custom.d/*.fish
   source $file
 end
 
+## Launch Starship
+if type -q starship
+  starship init fish | source
+end
+
 ## Launch The Fuck
 if type -q thefuck
   thefuck --alias | source
